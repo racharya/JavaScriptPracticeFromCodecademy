f89bf0c69a15218c54ca4ed1c48da7a885cc4df5
Rachana Acharya <rasnacharya@gmail.com>
2017-01-04 16:28:37 -0700

    Initial file

---
+++ types.js
@@ -0,0 +1,9 @@
+var myString = undefined;
+var myNumber = undefined;
+var myBoolean = undefined;
+
+
+// Do not edit the code under this line
+console.log("Name: " + myString);
+console.log("Lucky Number: " + myNumber);
+console.log("Good joke? " + myBoolean);
