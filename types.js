3184b372d4e955adfbb004de46a4f7cae8079ffe
Rachana Acharya <rasnacharya@gmail.com>
2017-01-04 16:33:08 -0700

    Changed each variable values

--- types.js
+++ types.js
@@ -1,6 +1,8 @@
-var myString = undefined;
-var myNumber = undefined;
-var myBoolean = undefined;
+//Replace each variable's value with each corresponding type
+//Replace myString with my name
+var myString = "John Doe";
+var myNumber = 7124598;//lucky number
+var myBoolean = true;//liked the joke
 
 
 // Do not edit the code under this line
