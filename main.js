f98d4b612f7b38c9914b99dc0fad769db329e3de
Rachana Acharya <rasnacharya@gmail.com>
2017-01-05 00:48:06 -0700

    Selected an HTML element and used its value on a pop-up

--- main.js
+++ main.js
@@ -1,1 +1,4 @@
-alert('Hello JavaScript!');
+//creating a variable named skillset and set it equal to the HTML element with the class skillset
+var skillset = document.getElementsByClassName('skillset');
+//Use an alert to make the skillset variable's value into a pop-up
+alert(skillset);//pop-up shows [object HTMLCollection]
