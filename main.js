dff5c0f012082b31380f135d1f8b4f6412fc1031
Rachana Acharya <rasnacharya@gmail.com>
2017-01-05 01:05:45 -0700

    Successfully used jQuery on the same alert pop-up used with JavaScript

--- main.js
+++ main.js
@@ -3,6 +3,9 @@
 //Use an alert to make the skillset variable's value into a pop-up
 //alert(skillset);//pop-up shows [object HTMLCollection]
 function main(){
-
+//Write a variable named $skillset and set it equal to jQuery selector for the skillset class
+var $skillset = $('.skillset');
+//write an alert on $skillset (variable)
+alert($skillset);
 }
 $(document).ready(main);
