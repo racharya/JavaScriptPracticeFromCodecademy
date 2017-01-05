5705d1fbbb256dbb5e5d1a9afcd13e8a56b79b66
Rachana Acharya <rasnacharya@gmail.com>
2017-01-05 00:57:28 -0700

    Getting ready to use jQuery

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 //creating a variable named skillset and set it equal to the HTML element with the class skillset
-var skillset = document.getElementsByClassName('skillset');
+//var skillset = document.getElementsByClassName('skillset');
 //Use an alert to make the skillset variable's value into a pop-up
-alert(skillset);//pop-up shows [object HTMLCollection]
+//alert(skillset);//pop-up shows [object HTMLCollection]
+function main(){
+
+}
+$(document).ready(main);
