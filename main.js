b1a839ec013864b7b7c8891b032265e2626175b5
Rachana Acharya <rasnacharya@gmail.com>
2017-01-05 01:10:46 -0700

    Used hide function to hide elements of class skillset

--- main.js
+++ main.js
@@ -2,10 +2,12 @@
 //var skillset = document.getElementsByClassName('skillset');
 //Use an alert to make the skillset variable's value into a pop-up
 //alert(skillset);//pop-up shows [object HTMLCollection]
-function main(){
-//Write a variable named $skillset and set it equal to jQuery selector for the skillset class
-var $skillset = $('.skillset');
-//write an alert on $skillset (variable)
-alert($skillset);
+function main() {
+  //Write a variable named $skillset and set it equal to jQuery selector for the skillset class
+  //var $skillset = $('.skillset');
+  //write an alert on $skillset (variable)
+  //alert($skillset);
+
+  $('.skillset').hide();
 }
 $(document).ready(main);
